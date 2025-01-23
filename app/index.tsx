f1a44d8e869b687ee066b47cf3d42d7506f8e1f7
Lucas <lucasvanderwielen@icloud.com>
2025-01-23 14:14:23 -0500

    Created a rough version of the landing page, no functionality but the layout is done, the description and name still need to be changed

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import Login from "./Login";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Login></Login>
+    </View>
+  );
+}
